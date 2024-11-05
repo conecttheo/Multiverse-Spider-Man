78f1a51250dc9e04ed7e6d437c4e820012122fb9
conecttheo <theodamata@gmail.com>
2024-11-04 22:34:24 -0300

    version 03

---
+++ assets/script/index.js
@@ -0,0 +1,34 @@
+function mudarBackground1() {
+    const bodySpider1 = document.querySelector('.s-spiderman-01-01');
+    bodySpider1.classList.remove('s-spiderman-01-01');
+
+}
+function mudarBackground2() {
+    const bodySpider2 = document.querySelector('.s-spiderman-01-02');
+    bodySpider2.classList.remove('s-spiderman-01-02');
+}
+function mudarBackground3() {
+    const bodySpider3 = document.querySelector('.s-spiderman-01-03');
+    bodySpider3.classList.remove('s-spiderman-01-03');
+}
+function mudarBackground4() {
+    const bodySpider4 = document.querySelector('.s-spiderman-02-01');
+    bodySpider4.classList.remove('s-spiderman-02-01');
+}
+function mudarBackground5() {
+    const bodySpider5 = document.querySelector('.s-spiderman-02-02');
+    bodySpider5.classList.remove('s-spiderman-02-02');
+}
+function mudarBackground6() {
+    const bodySpider6 = document.querySelector('.s-spiderman-03-01');
+    bodySpider6.classList.remove('s-spiderman-03-01');
+}
+function mudarBackground7() {
+    const bodySpider7 = document.querySelector('.s-spiderman-03-02');
+    bodySpider7.classList.remove('s-spiderman-03-02');
+}
+function mudarBackground8() {
+    const bodySpider8 = document.querySelector(' .s-spiderman-03-03');
+    bodySpider8.classList.remove('s-spiderman-03-03');
+
+}
